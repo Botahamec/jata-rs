ec18a08c7143f2db1468014de0246e8a218cbc3b
mrw1593 <botahamec@outlook.com>
2020-01-31 22:48:45 -0500

    Used ToStr and FromStr traits so the JataType trait is no longer necessary
    The return types of the methods have changed to satisfy this. The Options have transitioned to Results.

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,24 @@
 
 use std::fs::read_to_string;
 use std::fs::write;
-
-/**
- * This allows a type to be used with Jata.
- * This makes sure that a file can be translated to a string.
- * It also needs to be able to parse a String into itself
- */
-pub trait JataType where Self: std::marker::Sized {
-
-	/**
-	 * This takes a string and converts it to the type.
-	 * Returns None if the String is not a correct representation of the type.
-	 */
-	fn read(representation: String) -> Option<Self>;
-
-	/**
-	 * Converts the type to a String.
-	*/
-	fn to_str(&self) -> String;
-}
-
+use std::io::Error;
+use std::str::FromStr;
 
 #[derive(Default)]
 /**
  * This is a struct containing a path to a file.
  * It takes a generic type which defines what type the file contains.
  */
-pub struct JataFile<T: JataType + Default> {
+pub struct JataFile<T: ToString + FromStr + Default> {
 
 	/** A path to the file */
-	path: String,
+	pub path: String,
 
 	/** What is currently believed to be the value */
-	value: T
+	pub value: T
 }
 
-impl<T> JataFile<T> where T: JataType + Default {
+impl<T> JataFile<T> where T: ToString + FromStr + Default {
 
 	/**
 	 * The default constructor which doesn't initiate any fields
@@ -47,42 +29,47 @@ impl<T> JataFile<T> where T: JataType + Default {
 
 	/**
 	 * Checks and returns the current value of the file.
-	 * Returns None if there is an error when reading the file,
-	 * or if the file isn't a valid representation of the type.
+	 * Returns Ok if everything is ok.
+	 * Returns Ok(Err) if there was a problem parsing the file.
+	 * Returns Err if there was a problem reading the file
 	 */
-	pub fn check_value(self) -> Option<T> {
-		match read_to_string(self.path) {
-			Ok(s) => T::read(s),
-			Err(_e) => None
+	pub fn check_value(&self) -> std::io::Result<Result<T, T::Err>> {
+		match read_to_string(&self.path) {
+			Ok(s) => Ok(T::from_str(&s)),
+			Err(e) => Err(e)
 		}
 	}
 
 	/**
 	 * Checks the value of the file and sets the current value if applicable.
-	 * Returns Some(()) if the check was successful.
-	 * Return None if there was a problem with reading or parsing the file.
+	 * Returns Ok if the check was successful.
+	 * Returns Err if there was a problem with reading the file.
+	 * Returns Ok(Err) if there was a problem parsing the file.
 	 */
-	pub fn reset_value(&mut self) -> Option<()> {
-		match read_to_string(self.path.clone()) {
-			Ok(s) => match T::read(s) {
-				Some(t) => {self.value = t; Some(())},
-				None => None
-			},
-			Err(_e) => None
+	pub fn reset_value(&mut self) -> std::io::Result<Result<(), T::Err>> {
+		match self.check_value() {
+			Ok(r) => match r {
+					Ok(t) => {
+						self.value = t;
+						Ok(Ok(()))
+					},
+					Err(e) => Ok(Err(e))
+				},
+			Err(e) => Err(Error::from(e))
 		}
 	}
 
 	/**
 	 * Sets the value and writes the value to the file.
-	 * Returns Some(()) if the write was successful
-	 * Returns None if there was an error in writing to the file
+	 * Returns Ok if the write was successful
+	 * Returns Err if there was an error in writing to the file
 	 */
-	pub fn set_value(&mut self, value: T) -> Option<()> {
+	pub fn set_value(&mut self, value: T) -> std::io::Result<()> {
 		self.value = value;
-		let string_rep = self.value.to_str();
+		let string_rep = self.value.to_string();
 		match write(self.path.clone(), string_rep) {
-			Ok(_o) => Some(()),
-			Err(_e) => None
+			Ok(_o) => Ok(()),
+			Err(e) => Err(e)
 		}
 	}
 }
