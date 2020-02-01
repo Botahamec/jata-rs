c12f8fd9af2ff875b27e17cc894be4fa02e5489e
mrw1593 <botahamec@outlook.com>
2020-01-31 20:48:49 -0500

    Redid everything using generics

--- src/lib.rs
+++ src/lib.rs
@@ -1,186 +1,21 @@
-/* JATA.RS
- * A library to handle object databases which are not stored in memory
- * Instead, the structures are stored in physical storage, using txt files
- * This is most likely not the most efficient way to do this
- * However, I was unable to find any crates which did a better job
- */
 
-use std::io::Result;
-use std::io::Error;
-use std::io::ErrorKind;
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs::read_to_string;
 
-#[derive(PartialEq)]
-enum JataType {
-	Str,
-	Int,
-	Float,
-	Bool,
-	StrList,
-	IntList,
-	FloatList,
-	BoolList
+pub trait JataType where Self: std::marker::Sized {
+	fn read(representation: String) -> Option<Self>;
+	fn to_str(self) -> String;
 }
 
-struct JataProp {
-	name: String,
-	location: String,
-	jtype: JataType,
+pub struct JataFile<T> where T: JataType {
+	path: String,
+	value: T
 }
 
-struct JataObject {
-	location: String,
-	properties: Vec<JataProp>
-}
-
-macro_rules! struct {
-	($name: ty) => {
-		struct $name {
-			name: String,
-			location: String,
-			properties: Vec<JataProp>
+impl<T> JataFile<T> where T: JataType {
+	pub fn check_value(self) -> Option<T> {
+		match read_to_string(self.path) {
+			Ok(s) => T::read(s),
+			Err(_e) => None
 		}
-
-		impl $name {
-			fn new(location: String) -> $name {
-				$name {name: stringify!($name), location: location, properties: Vec::new()}
-			}
-		}
-	};
-}
-
-struct!{ServerInfo}
-
-impl JataProp {
-
-	fn new_raw(name: String, location: String, jtype: JataType, value: String) -> Result<JataProp> {
-		let mut file = File::create(location.clone())?;
-    	file.write_all(value.as_bytes())?;
-    	Ok(JataProp{name: name, location: location.clone(), jtype: jtype})
-		ServerInfo::new()
-	}
-
-	fn new_str(name: String, location: String, value: String) -> Result<JataProp> {
-		JataProp::new_raw(name, location, JataType::Str, value)
-	}
-
-	fn new_int(name: String, location: String, value: isize) -> Result<JataProp> {
-		JataProp::new_raw(name, location, JataType::Int, format!("{}", value))
-	}
-
-	fn new_float(name: String, location: String, value: f32) -> Result<JataProp> {
-		JataProp::new_raw(name, location, JataType::Float, format!("{}", value))
-	}
-
-	fn new_bool(name: String, location: String, value: bool) -> Result<JataProp> {
-		JataProp::new_raw(name, location, JataType::Bool, match value {
-			false => String::from("0"),
-			true => String::from("1")
-		})
-	}
-
-	fn new_str_list(name: String, location: String, value: Vec<String>) -> Result<JataProp> {
-		JataProp::new_raw(name, location, JataType::StrList, value.join("\n"))
-	}
-
-	fn new_int_list(name: String, location: String, value: Vec<isize>) -> Result<JataProp> {
-		JataProp::new_raw(name, location, JataType::IntList,
-							value.iter()
-							.map(|i| format!("{}", i))
-							.collect::<Vec<String>>()
-							.join("\n"))
-	}
-
-	fn new_float_list(name: String, location: String, value: Vec<f32>) -> Result<JataProp> {
-		JataProp::new_raw(name, location, JataType::FloatList,
-							value.iter()
-							.map(|f| format!("{}", f))
-							.collect::<Vec<String>>()
-							.join("\n"))
-	}
-
-	fn new_bool_list(name: String, location: String, value: Vec<bool>) -> Result<JataProp> {
-		JataProp::new_raw(name, location, JataType::BoolList,
-							value.iter()
-							.map(|b| match b {
-								false => String::from("0"),
-								true => String::from("1")
-							})
-							.collect::<Vec<String>>()
-							.join("\n"))
-	}
-
-	fn get_raw(&self) -> Result<String> {
-		let mut file = File::open(self.location.clone())?;
-    	let mut contents = String::new();
-    	file.read_to_string(&mut contents)?;
-    	Ok(contents)
-	}
-
-	fn get_raw_result(&self, jtype: JataType) -> Result<String> {
-		if self.jtype == jtype {
-			self.get_raw()
-		}
-		else {
-			Err(Error::new(
-				ErrorKind::InvalidData,
-				format!("The data at {} is not of type: {}",
-						self.location,
-						stringify!(jtype))
-			))
-		}
-	}
-
-	fn get_str(&self) -> Result<String> {
-		self.get_raw_result(JataType::Str)
-	}
-
-	fn get_int(&self) -> Result<isize> {
-		match self.get_raw_result(JataType::Int) {
-			Ok(s) => {
-				match s.parse::<isize>() {
-					Ok(i) => Ok(i),
-					Err(e) => Err(Error::new(ErrorKind::InvalidData, e))
-				}
-			}
-			Err(e) => Err(e)
-		}
-	}
-
-	fn get_float(&self) -> Result<f32> {
-		match self.get_raw_result(JataType::Int) {
-			Ok(s) => {
-				match s.parse::<f32>() {
-					Ok(f) => Ok(f),
-					Err(e) => Err(Error::new(ErrorKind::InvalidData, e))
-				}
-			}
-			Err(e) => Err(e)
-		}
-	}
-
-	fn get_bool(&self) -> Result<bool> {
-		match self.get_raw_result(JataType::Bool) {
-			Ok(s) => {
-				match s.as_str() {
-					"0" => Ok(false),
-					"1" => Ok(true),
-					_ => Err(Error::new(
-						ErrorKind::InvalidData,
-						format!("The file at {} did not contain 0 or 1",
-								self.location)
-					))
-				}
-			}
-			Err(e) => Err(e)
-		}
-	}
-
-	fn get_str_list(&self) -> Result<Vec<String>> {
-		let vec = self.get_raw_result(JataType::StrList)?;
-		Ok(vec.split_terminator("\n")
-			.map(|s| String::from(s))
-			.collect::<Vec<String>>())
 	}
 }
